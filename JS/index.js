e6b77987666cc967b71754b664904039fafc748c
Caelo <mike22541678@gmail.com>
2023-06-05 16:50:52 -0500

    JS add

---
+++ JS/index.js
@@ -0,0 +1,59 @@
+let dinoHola = "owo";
+
+console.log(dinoHola);
+
+function holiwi(nombresito){
+    console.log("Oa, " + nombresito + " ♥");
+}
+
+holiwi("Juan alverto jimenes perez san pedro")
+
+for(dinoHola = 0; dinoHola < 10; dinoHola++){
+    console.log("Tu owosidad es de " + dinoHola)
+}
+
+console.log(dinoHola)
+
+while(dinoHola > 0){
+    console.log("Tu owosidad disminuyo nub, ahora es de " + dinoHola);
+    dinoHola--;
+}
+
+let lolDivision = "gold";
+let champLol;
+
+switch (lolDivision) {
+  case "bronze":
+    champLol = "Teemo";
+    break;
+  case "silver":
+    champLol = "Darius";
+    break;
+  case "gold":
+    champLol = "Azir";
+    break;
+  case "platinum":
+    champLol = "Aphelios";
+    break;
+  case "diamond":
+    champLol = "Bardo";
+    break;
+  case "challenger":
+    champLol = "Akshan";
+    break;
+  case "fakerxd":
+    champLol = "Le Blanc";
+    break;
+  default:
+    champLol = "Día inválido";
+}
+
+console.log("Tu champ es " + champLol);
+
+console.log(dinoHola)
+
+if(dinoHola == 0){
+    console.log("JAJAJAJA tu owosidad es de 0, mero nub xd")
+} else{
+    console.log("Weno, tienes más owosidad que 0, no eres nub uwu")
+}
