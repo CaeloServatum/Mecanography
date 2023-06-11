f733ccae9b15bbea801e8bf9897f6e01bb65af1c
Caelo <mike22541678@gmail.com>
2023-06-11 13:13:51 -0500

    Js create and page function correctly

--- JS/index.js
+++ JS/index.js
@@ -1,29 +1,163 @@
 window.addEventListener('DOMContentLoaded',function(){
 
-    const eye = document.querySelector('.fa-eye')
-    const eyeSlash = document.querySelector('.fa-eye-slash')
-    const timer = document.querySelector('.timer_Buttton p')
+    
+    const textDiv = document.querySelector(".text_square");
+    const eyeButton = document.querySelector('.eye_Button');
+    const timer = document.querySelector('.timer_Buttton');
+    const time = document.querySelector('.timer_Buttton span');
+    const scoreDiv = document.querySelector(".score_Square");
+    const eye = document.querySelector(".fa-eye");
+    const eyeSlash = document.querySelector(".fa-eye-slash");
+    const input = document.querySelector('.main_Input input');
+    const correctSquare = document.querySelector('.correct_Square p');
+    const wrongSquare = document.querySelector('.wrong_Square p');
+    const puntuacion = document.querySelector('.puntuacion')
 
-    console.log(eye);
-    console.log(eyeSlash);
-    console.log(timer);
+    let counterStarted = false;
+    let initialWord = 0;
+    let timerCounter = 60;
+    let selectedPharagraph;
+    let correctWords = 0;
+    let wrongWords = 0;
 
-    eye.addEventListener('click',function(){ 
-        if (eye.style.display == flex) {
-            esconder("hidden")
-        } else {
-            esconder("none")
+    
+
+    input.addEventListener('keydown',function(e){
+
+        if (counterStarted == false) {
+            counterStarted = true;
+            let timerInterval = setInterval(function(){
+                timerCounter --;
+                timer.children[0].textContent = "0:"+timerCounter;
+                if(timerCounter == 0) {
+                    puntuacion.textContent = 'tu puntuacion es: '+correctWords+' WPM'
+                    puntuacion.classList.remove('d-none');
+                    setTimeout(() => {
+                        puntuacion.classList.add('d-none');
+                    }, 5000);
+                }
+            },1000)
         }
-    eyeSlash.addEventListener('click',function(){
-        eyeSlash.style.display = flex;
-    })
-    timer.addEventListener('click',function(){
-        timer.style.display = none;
+
+     
+        const textSpans = document.querySelectorAll(".text_square span");
+
+       
+
+        if(e.keyCode == 32) {
+
+            if(textSpans[initialWord].textContent == e.target.value.trim()) {
+                textSpans[initialWord].classList.add('correct')
+                textSpans[initialWord].classList.remove('wrong')
+                correctWords ++;
+                correctSquare.textContent = correctWords
+            } else {
+                textSpans[initialWord].classList.add('wrong')
+                textSpans[initialWord].classList.remove('correct')
+                wrongWords ++;
+                wrongSquare.textContent = wrongWords
+
+            }
+
+            e.target.value = '';
+            initialWord++;
+            textSpans[initialWord].classList.add('active');
+
+            
+        }
     })
 
-    function esconder(estado){
-        eye.style.display = estado;
+
+    let words = [
+        'La salchichita espacial es una comida muy especial para los gatitos espaciales por que tiene mucha carne espacial, es la que se necesita para que sus cuerpos espaciales aguanten el espacio espaciado por que los gatitos espaciales tienen muchas michi dificultades en su michiespacial vida.',
+        'League of Legends (también conocido por sus siglas LoL), es un videojuego multijugador de arena de batalla en línea desarrollado y publicado por Riot Games. Inspirándose en Defense of the Ancients, los fundadores de Riot buscaron desarrollar un juego independiente del mismo género.',
+        'Steven Universe es una serie de televisión estadounidense, en formato de animación, creada por Rebecca Sugar para Cartoon Network. Narra la historia de un niño llamado Steven, que vive en el pueblo ficticio de Beach City junto a tres guerreras alienígenas con superpoderes: Garnet, Amatista y Perla. A lo largo de los episodios, Steven aprenderá a controlar los poderes de su gema de cuarzo heredados de su madre y ayudará al resto de gemas en las misiones para defender a la humanidad.'
+    ]
+
+    createSpans()
+
+
+   
+
+    let localStorage = window.localStorage;
+    // Variables Inicializada ♥
+    let showTimer;
+    let showScore;
+    
+    // ---- GETTING LOCAL STORAGE ----
+    showScore = localStorage.getItem('showScore') || 'false';
+    showScore = showScore === 'true';
+
+    // validando si hay una llave 'showTimer' en el local storage
+    showTimer = localStorage.getItem("showTimer") || "true"; // valor que se recibe del localStorage
+    showTimer = showTimer === "true";
+    // --------
+
+
+    if(showTimer) {
+        timer.children[0].classList.remove("d-none")
+    } else {
+        timer.children[0].classList.add("d-none")
     }
+
+    if(showScore) {
+        scoreDiv.classList.remove("d-none")
+        eyeButton.children[0].classList.add('d-none');
+        eyeButton.children[1].classList.remove('d-none');
+    } else {
+        scoreDiv.classList.add("d-none")
+        eyeButton.children[0].classList.remove('d-none');
+        eyeButton.children[1].classList.add('d-none');
+        
+    }
+                                // Funcion implicita ♥
+    eyeButton.addEventListener('click',function(){
+        mostrar();
+    })
+
+    timer.addEventListener('click',function(){
+        showTimer = !showTimer;
+        localStorage.setItem('showTimer',showTimer)
+
+    
+        timer.children[0].classList.toggle('d-none');
+    })
+
+    function mostrar() {
+
+        showScore = !showScore 
+        localStorage.setItem('showScore',showScore)
+
+        eyeButton.children[0].classList.toggle('d-none');
+        eyeButton.children[1].classList.toggle('d-none');
+
+        scoreDiv.classList.toggle("d-none");
+
+    }
+
+
+    function createSpans() {
+
+        
+        selectedPharagraph = words[Math.floor(Math.random(3) * 3)]
+
+        console.log(selectedPharagraph)
+    
+        selectedPharagraph = selectedPharagraph.split(' ')
+       
+        selectedPharagraph.forEach(function(element){
+    
+            let span = document.createElement('span')
+            span.textContent = element
+            
+    
+            textDiv.appendChild(span)
+        })
+    
+        textDiv.children[0].classList.add('active')
+    }
+
+
 
     // function ponerColores (color,letra,aumentar) {
 
@@ -48,7 +182,7 @@ window.addEventListener('DOMContentLoaded',function(){
         //     } else {
         //       text.children[0].style.fontSize = pixeles - 1 + "px"
         //     }
-})
+// })
 
 // const rojo = document.querySelector('#home')
 // rojo.classList.add('clasesita')
